d85d74416f3414eee4f0d6f5363fe6db7dbdbc6c
Pawel W <elendil@konto.pl>
2018-04-13 22:13:39 +0200

    Second stuff, which basically works

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+How to start redis docker image, to get correct communication
+
+sudo docker run -d -p 6379:6379 redis
--- storage.js
+++ storage.js
@@ -1,6 +1,10 @@
 var redis = require('redis');
 var client = redis.createClient();
 
-client.on(error, function( err) {
+client.on('error', function( err) {
     console.log((new Date()), 'Redis Error' + JSON.stringify(err));
 });
+
+client.get("firstValue", function(err, reply ) {
+    console.log(reply);
+} );
